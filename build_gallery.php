944550fef168ede1d047405651bdb92a526177df
Nicholas Pettas <npettas@gmail.com>
2018-01-04 20:19:54 -0800

    remove imagizer from images. the images will need to be cropped by hand now.

--- build_gallery.php
+++ build_gallery.php
@@ -5,8 +5,8 @@ $files = glob(dirname(__FILE__) . "/img/photos/*");
 foreach($files as $file) {
     $file = pathinfo($file, PATHINFO_BASENAME);
     echo '<div class="gallery-image col-lg-4 col-md-4 col-sm-4 col-xs-6">';
-    echo '<a class="swipebox" rel="gallery-lg" href="https://cassiascorner.imagizer.com/img/photos/' . $file . '">';
-    echo '<img class="img-responsive" src="https://cassiascorner.imagizer.com/img/photos/'. $file .'?crop=fit&height=400&width=500">';
+    echo '<a class="swipebox" rel="gallery-lg" href="img/photos/' . $file . '">';
+    echo '<img class="img-responsive" src="img/photos/'. $file . '>';
     echo '</a>';
     echo '</div>';
 }
